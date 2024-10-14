db4fc00e84c0e9fa6f5cac16b3ad9255b7516a20
fuadlabibalh <fuadlabibal@gmail.com>
2024-10-14 11:44:36 +0700

    fix: export

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import useDeviceScreen from "./device_screen/useDeviceScreen"
 import { useCssGenerator } from "./style_sheet/useCssGenerator"
 import { useResponsive } from "./style_sheet/useResponsive"
-
-export default {
+const resp = {
     useResponsive,
     useCssGenerator,
     useDeviceScreen
 }
+
+
+export default resp
---
+++ test.ts
@@ -0,0 +1,1 @@
+import { useResponsive } from "./style_sheet/useResponsive";
