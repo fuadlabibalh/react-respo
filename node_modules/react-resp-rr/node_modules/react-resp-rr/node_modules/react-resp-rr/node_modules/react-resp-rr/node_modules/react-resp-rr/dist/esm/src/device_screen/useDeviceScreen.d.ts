c93c4435aa0229fb244926aa942a8a21a7436647
fuadlabibalh <164131210+fuadlabibalh@users.noreply.github.com>
2024-10-24 12:35:14 +0700

    Delete node_modules directory
    Signed-off-by: fuadlabibalh <164131210+fuadlabibalh@users.noreply.github.com>

--- node_modules/react-resp-rr/node_modules/react-resp-rr/node_modules/react-resp-rr/node_modules/react-resp-rr/node_modules/react-resp-rr/dist/esm/src/device_screen/useDeviceScreen.d.ts
+++
@@ -1,4 +0,0 @@
-import { DeviceScreens } from '.';
-declare function useDeviceScreen(): DeviceScreens;
-export declare const useDimension: () => import(".").ScreenDimension;
-export default useDeviceScreen;
