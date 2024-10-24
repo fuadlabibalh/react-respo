d43cff86f9b5b6ecb66433797e25b3079a754514
fuadlabibalh <fuadlabibal@gmail.com>
2024-10-24 12:37:34 +0700

    feat:final

--- dist/esm/index.js
+++
@@ -1,9 +0,0 @@
-import useDeviceScreen from "./device_screen/useDeviceScreen";
-import { useCssGenerator } from "./style_sheet/useCssGenerator";
-import { useResponsive } from "./style_sheet/useResponsive";
-const cssrr = {
-    useResponsive,
-    useCssGenerator,
-    useDeviceScreen
-};
-export default cssrr;
